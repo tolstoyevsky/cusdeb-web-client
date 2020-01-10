266bfa48945323232b2c9eb9b4aa59379d70bef9
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-01-10 12:14:34 +0300

    Make title and main property of state config not a function

--- src/modules/Builder/components/Builder/config.js
+++ src/modules/Builder/components/Builder/config.js
@@ -1,38 +1,36 @@
-import React from "react";
-
 import Initialization from "modules/Builder/components/Initialization/Initialization";
 
 const routeBasePath = "/builder";
 const routes = [
     {
         path: `${routeBasePath}/initialization`,
-        title: () => "Initialization",
-        main: () => <Initialization />,
+        title: "Initialization",
+        main: Initialization,
     },
     {
         path: `${routeBasePath}/desktop`,
-        title: () => "Desktop environment",
-        main: () => "",
+        title: "Desktop environment",
+        main: "",
     },
     {
         path: `${routeBasePath}/packagelist`,
-        title: () => "Package list",
-        main: () => "",
+        title: "Package list",
+        main: "",
     },
     {
         path: `${routeBasePath}/usergroups`,
-        title: () => "User and groups",
-        main: () => "",
+        title: "User and groups",
+        main: "",
     },
     {
         path: `${routeBasePath}/configuration`,
-        title: () => "Configuration",
-        main: () => "",
+        title: "Configuration",
+        main: "",
     },
     {
         path: `${routeBasePath}/build`,
-        title: () => "Build",
-        main: () => "",
+        title: "Build",
+        main: "",
     },
 ];
 
