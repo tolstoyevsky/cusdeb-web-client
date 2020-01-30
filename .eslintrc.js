e2d79b4ed5d384f22cd0b77457fadb45e39e2238
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-01-31 00:11:25 +0300

    Set up Travis CI for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        "plugin:react/recommended",
+        "airbnb",
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    plugins: [
+        "react",
+    ],
+    rules: {
+        quotes: [
+            "error",
+            "double"
+        ],
+        indent: [
+            "error",
+            4,
+            { SwitchCase: 1 },
+        ],
+        "react/jsx-indent": [
+            "error",
+            4,
+        ],
+        "import/prefer-default-export": "off",
+        "react/jsx-indent-props": [
+            "error",
+            4
+        ],
+        "arrow-body-style": [
+            "error",
+            "as-needed",
+        ],
+        "react/destructuring-assignment": [
+            "error",
+            "always",
+            {
+                ignoreClassFields: true
+            },
+        ]
+    },
+    settings: {
+        react: {
+            pragma: "React",
+            version: "16.12.0"
+        },
+        "import/resolver": {
+            node: {
+                paths: ["src"]
+            }
+        }
+    }
+};
