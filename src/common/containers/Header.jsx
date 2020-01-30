934fc35882fb969e660f0780d1d9f43d7d753473
IarovoyVlad <vlad_yarovoy_97@mail.ru>
2020-01-30 18:45:53 +0300

    Implement dropdown menu by clicking on username in header

--- src/common/containers/Header.jsx
+++ src/common/containers/Header.jsx
@@ -2,15 +2,22 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { signOut } from "utils/users";
 
 import * as API from "api/http/users";
 
 export default class Header extends Component {
+    static logOut(event) {
+        event.preventDefault();
+        return signOut();
+    }
+
     constructor(props) {
         super(props);
 
         this.state = {
             user: {},
+            userIsAuth: false,
         };
     }
 
@@ -18,12 +25,13 @@ export default class Header extends Component {
         API.whoAmI().then((response) => {
             this.setState(() => ({
                 user: response.data,
+                userIsAuth: response.status === 200,
             }));
         });
     }
 
     render() {
-        const { user } = this.state;
+        const { user, userIsAuth } = this.state;
         const { logo, pushmenu, styleName } = this.props;
         return (
             <nav className={`main-header navbar navbar-expand navbar-light navbar-white ${styleName}`}>
@@ -52,10 +60,28 @@ export default class Header extends Component {
                     <li className="nav-item">
                         <a href="/blog" className="nav-link">Blog</a>
                     </li>
-                    <li className="nav-item">
-                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a href="#" className="nav-link">{user.username}</a>
-                    </li>
+                    {!userIsAuth ? (
+                        <li className="nav-item">
+                            <a href="/signin" className="nav-link">Sign in</a>
+                        </li>
+                    )
+                        : (
+                            <li className="nav-item dropdown user-menu ">
+                                <button type="button" className="btn nav-link dropdown-toggle" id="dLabel" data-target="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {user.username}
+                                </button>
+                                <ul className="dropdown-menu dropdown-menu-lg dropdown-menu-right" aria-labelledby="dLabel">
+                                    <li className="user-header bg-primary dropdown-item">
+                                        <p>{user.username}</p>
+                                    </li>
+                                    <li className="user-footer dropdown-item">
+                                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                        <a href="#" className="btn btn-default btn-flat">Profile</a>
+                                        <button type="button" onClick={Header.logOut} className="btn btn-default btn-flat float-right">Sign out</button>
+                                    </li>
+                                </ul>
+                            </li>
+                        )}
                 </ul>
             </nav>
         );
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -6,5 +6,6 @@ import App from "./App";
 import "styles/index.scss";
 // eslint-disable-next-line import/no-unresolved
 import "admin-lte-js";
+import "bootstrap";
 
 ReactDOM.render(<App />, document.getElementById("root"));
