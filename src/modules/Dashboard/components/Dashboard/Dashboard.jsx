9e4b18d5ac09e1ea17ff9c68645215f6e2d2a308
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-02-10 10:38:56 +0300

    Add content-header on Dashboard

--- src/modules/Dashboard/components/Dashboard/Dashboard.jsx
+++ src/modules/Dashboard/components/Dashboard/Dashboard.jsx
@@ -2,12 +2,39 @@ import React, { Component } from "react";
 
 import Regular from "common/containers/Regular";
 
+import { Button } from "react-bootstrap";
+import { faWrench } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Dashboard extends Component {
     render() {
         return (
             <Regular>
-                {" "}
+                <section className="content-header">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <h3 className="m-0 text-dark">
+                                    Control Panel
+                                    <small className="text-gray">
+                                        My Images
+                                    </small>
+                                </h3>
+                            </div>
+                            <div className="col-sm-6">
+                                <ol className="breadcrumb float-sm-right">
+                                    <li className="breadcrumb-item">
+                                        <Button variant="primary">
+                                            <FontAwesomeIcon icon={faWrench} />
+                                            Create New
+                                        </Button>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </Regular>
         );
     }
