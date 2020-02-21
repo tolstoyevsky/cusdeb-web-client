699aeca685a0c26130d7f9bc6e2d4e0c39f91606
IarovoyVlad <vlad_yarovoy_97@mail.ru>
2020-02-21 15:45:48 +0300

    Add icons of image build stages to sidebar

--- src/common/containers/Sidebar.jsx
+++ src/common/containers/Sidebar.jsx
@@ -4,14 +4,19 @@ import PropTypes from "prop-types";
 const Sidebar = ({ children }) => (
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
         <a href="/" className="brand-link">
-            <center>
-                CusDeb
-                <sup>beta</sup>
+            <center className="nav-item">
+                C
+                <span className="brand-text show">us</span>
+                D
+                <span className="brand-text show">
+                    eb
+                    <sup>beta</sup>
+                </span>
             </center>
         </a>
         <div className="sidebar">
             <nav className="mt-2">
-                <ul className="nav nav-pills nav-treeview flex-column">
+                <ul className="nav nav-sidebar nav-pills nav-treeview flex-column" data-widget="treeview" data-according="false">
                     {children.map((item, index) => (
                         // eslint-disable-next-line react/no-array-index-key
                         <li className="nav-item" key={index}>
--- src/modules/Builder/components/Builder/config.js
+++ src/modules/Builder/components/Builder/config.js
@@ -1,5 +1,12 @@
 import Initialization from "modules/Builder/components/Initialization/Initialization";
 import PackageList from "modules/Builder/components/PackageList/PackageList";
+import {
+    faMicrochip,
+    faList,
+    faUsers,
+    faCogs,
+    faWrench,
+} from "@fortawesome/free-solid-svg-icons";
 
 const routeBasePath = "/builder";
 const routes = [
@@ -7,26 +14,36 @@ const routes = [
         path: `${routeBasePath}/initialization`,
         title: "Initialization",
         main: Initialization,
+        icon: faMicrochip,
+        icon_style: "",
     },
     {
         path: `${routeBasePath}/packagelist`,
         title: "Package list",
         main: PackageList,
+        icon: faList,
+        icon_style: "",
     },
     {
         path: `${routeBasePath}/usergroups`,
         title: "User and groups",
         main: "",
+        icon: faUsers,
+        icon_style: "",
     },
     {
         path: `${routeBasePath}/configuration`,
         title: "Configuration",
         main: "",
+        icon: faCogs,
+        icon_style: "",
     },
     {
         path: `${routeBasePath}/build`,
         title: "Build",
         main: "",
+        icon: faWrench,
+        icon_style: "fa-flip-horizontal",
     },
 ];
 
