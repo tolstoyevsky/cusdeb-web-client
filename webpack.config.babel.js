3764ae406ea8958e58aa6b03313901d4f0e52cf2
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-02-21 13:01:43 +0300

    Add sass-loader and node-sass in dependencies

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -52,6 +52,7 @@ module.exports = {
                 use: [
                     "style-loader",
                     "css-loader",
+                    "sass-loader",
                 ],
             },
         ],
