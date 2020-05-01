e9703a733da656a00ac807e8ef221d8effeccbc7
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-05-01 14:57:07 +0300

    Improve processing of stages in Builder

--- src/modules/Builder/components/Builder/config.js
+++ src/modules/Builder/components/Builder/config.js
@@ -1,9 +1,3 @@
-import Build from "modules/Builder/components/Build/Build";
-import Configuration from "modules/Builder/components/Configuration/Configuration";
-import Initialization from "modules/Builder/components/Initialization/Initialization";
-import PackageList from "modules/Builder/components/PackageList/PackageList";
-import Users from "modules/Builder/components/Users/Users";
-
 import {
     faMicrochip,
     faList,
@@ -12,45 +6,51 @@ import {
     faWrench,
 } from "@fortawesome/free-solid-svg-icons";
 
+import Build from "modules/Builder/components/Build/Build";
+import Configuration from "modules/Builder/components/Configuration/Configuration";
+import Initialization from "modules/Builder/components/Initialization/Initialization";
+import PackageList from "modules/Builder/components/PackageList/PackageList";
+import Users from "modules/Builder/components/Users/Users";
+
 const routeBasePath = "/builder";
-const routes = [
-    {
+const BUILDER_STAGES = {
+    initialization: {
         path: `${routeBasePath}/initialization`,
         title: "Initialization",
         main: Initialization,
         icon: faMicrochip,
         icon_style: "",
     },
-    {
+    packagelist: {
         path: `${routeBasePath}/packagelist`,
         title: "Package list",
         main: PackageList,
         icon: faList,
         icon_style: "",
     },
-    {
+    users: {
         path: `${routeBasePath}/users`,
         title: "Users",
         main: Users,
         icon: faUsers,
         icon_style: "",
     },
-    {
+    configuration: {
         path: `${routeBasePath}/configuration`,
         title: "Configuration",
         main: Configuration,
         icon: faCogs,
         icon_style: "",
     },
-    {
+    build: {
         path: `${routeBasePath}/build`,
         title: "Build",
         main: Build,
         icon: faWrench,
         icon_style: "fa-flip-horizontal",
     },
-];
+};
 
-const baseRouteIndex = 0;
+const NEXT_BUTTON_INACTIVE_STAGES = ["build"];
 
-export { routes, baseRouteIndex };
+export { BUILDER_STAGES, NEXT_BUTTON_INACTIVE_STAGES };
