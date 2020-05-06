2c97e502db2df0ab3e8bf7100ee89547d734b695
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-05-06 22:14:40 +0300

    Move package resolving logic to PackagesTable component

--- src/modules/Builder/components/PackageList/PackageList.jsx
+++ src/modules/Builder/components/PackageList/PackageList.jsx
@@ -9,42 +9,7 @@ export default class PackageList extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            basePackages: [],
-            selectedPackages: [],
-            dependentPackages: [],
-        };
-
         this.blackmagic = new Blackmagic();
-
-        this.resolvePackage = this.resolvePackage.bind(this);
-    }
-
-    componentDidMount() {
-        this.blackmagic.fetchBasePackagesList()
-            .then((basePackages) => {
-                this.setState(() => ({ basePackages }));
-            });
-    }
-
-    resolvePackage(packageName, action) {
-        const { selectedPackages } = this.state;
-        const newSelectedPackages = [...selectedPackages];
-
-        if (action === "add") {
-            newSelectedPackages.push(packageName);
-        } else if (action === "remove") {
-            newSelectedPackages.pop(packageName);
-        }
-
-        this.setState(() => ({
-            selectedPackages: newSelectedPackages,
-        }));
-
-        this.blackmagic.resolvePackages(newSelectedPackages)
-            .then((dependentPackages) => {
-                this.setState(() => ({ dependentPackages }));
-            });
     }
 
     executeState() {
@@ -53,15 +18,8 @@ export default class PackageList extends Component {
     }
 
     render() {
-        const { basePackages, selectedPackages, dependentPackages } = this.state;
-        const packages = {
-            base: basePackages,
-            selected: selectedPackages,
-            dependent: dependentPackages,
-        };
-
         return (
-            <PackagesTable key="packages-list-table" packages={packages} resolvePackage={this.resolvePackage} />
+            <PackagesTable key="packages-list-table" />
         );
     }
 }
