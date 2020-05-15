544ff33119270e0fa4b49b8bdc6d94ec591f6bb0
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-05-15 08:41:57 +0300

    Replace local components for Card from react-bootstrap

--- src/modules/SignIn/components/SignIn/SignIn.jsx
+++ src/modules/SignIn/components/SignIn/SignIn.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 
 import { addBodyClass } from "utils/misk";
 
-import Card from "common/containers/Card";
 import FormSignIn from "./components/FormSignIn/FormSignIn";
 
 export default class SignIn extends Component {
@@ -13,12 +13,16 @@ export default class SignIn extends Component {
     render() {
         return (
             <div className="login-box">
-                <Card additionalClasses="login-card-body">
-                    <p className="login-box-msg">Sign in to build an image</p>
-                    <FormSignIn />
-                    <p className="mb-0 mt-3">
-                        <a href="/signup">Register a new account</a>
+                <Card className="login-card-body pb-0">
+                    <p className="login-box-msg">
+                        Sign in to build an image
                     </p>
+                    <Card.Body>
+                        <FormSignIn />
+                        <p className="mb-0 mt-3">
+                            <a href="/signup">Register a new account</a>
+                        </p>
+                    </Card.Body>
                 </Card>
             </div>
         );
