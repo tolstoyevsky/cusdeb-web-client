c97e76789ee2fa7f0e9b680f653ca05b35de57e8
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-09-05 21:51:26 +0300

    Rework header using react-bootstrap

--- src/common/containers/Header.jsx
+++ src/common/containers/Header.jsx
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { signOut } from "utils/users";
+import {
+    Container,
+    Dropdown,
+    Nav,
+    Navbar,
+} from "react-bootstrap";
 
 import * as API from "api/http/users";
+import { signOut } from "utils/users";
 
 export default class Header extends Component {
-    static logOut(event) {
-        event.preventDefault();
-        return signOut();
-    }
-
     constructor(props) {
         super(props);
 
@@ -31,59 +32,49 @@ export default class Header extends Component {
     }
 
     render() {
+        const { logo, pushmenu } = this.props;
         const { user, userIsAuth } = this.state;
-        const { logo, pushmenu, styleName } = this.props;
         return (
-            <nav className={`main-header navbar navbar-expand navbar-light navbar-white ${styleName}`}>
-                {logo && (
-                    <a href="/" className="navbar-brand">
-                        Cusdeb
-                        <sup>beta</sup>
-                    </a>
-                )}
-
-                {pushmenu && (
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <a href="#" data-target="" data-toggle="collapse" data-widget="pushmenu" className="nav-link">
+            <Navbar variant="white" className="main-header">
+                <Container>
+                    {logo && (
+                        <Navbar.Brand className="text-dark" href="/">
+                            CusDeb
+                            <sup>beta</sup>
+                        </Navbar.Brand>
+                    )}
+                    {pushmenu && (
+                        <Nav>
+                            <Nav.Link
+                                className="text-dark"
+                                data-target=""
+                                data-toggle="collapse"
+                                data-widget="pushmenu"
+                                href="#"
+                            >
                                 <FontAwesomeIcon icon={faBars} />
-                            </a>
-                        </li>
-                    </ul>
-                )}
-
-                <ul className="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
-                    <li className="nav-item">
-                        <a href="/dashboard" className="nav-link">Dashboard</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="/blog" className="nav-link">Blog</a>
-                    </li>
-                    {!userIsAuth ? (
-                        <li className="nav-item">
-                            <a href="/signin" className="nav-link">Sign in</a>
-                        </li>
-                    )
-                        : (
-                            <li className="nav-item dropdown user-menu ">
-                                <button type="button" className="btn nav-link dropdown-toggle" id="dLabel" data-target="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            </Nav.Link>
+                        </Nav>
+                    )}
+                    <Nav className="ml-auto">
+                        <Nav.Link className="text-dark" href="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link className="text-dark" href="/blog">Blog</Nav.Link>
+                        {userIsAuth ? (
+                            <Dropdown as={Nav.Item} alignRight>
+                                <Dropdown.Toggle className="text-dark" as={Nav.Link}>
                                     {user.username}
-                                </button>
-                                <ul className="dropdown-menu dropdown-menu-lg dropdown-menu-right" aria-labelledby="dLabel">
-                                    <li className="user-header bg-primary dropdown-item">
-                                        <p>{user.username}</p>
-                                    </li>
-                                    <li className="user-footer dropdown-item">
-                                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                        <a href="#" className="btn btn-default btn-flat">Profile</a>
-                                        <button type="button" onClick={Header.logOut} className="btn btn-default btn-flat float-right">Sign out</button>
-                                    </li>
-                                </ul>
-                            </li>
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                                    <Dropdown.Item onClick={signOut}>Sign out</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        ) : (
+                            <Nav.Link className="text-dark" href="/signin">Sign in</Nav.Link>
                         )}
-                </ul>
-            </nav>
+                    </Nav>
+                </Container>
+            </Navbar>
         );
     }
 }
@@ -91,11 +82,9 @@ export default class Header extends Component {
 Header.propTypes = {
     logo: PropTypes.bool,
     pushmenu: PropTypes.bool,
-    styleName: PropTypes.string,
 };
 
 Header.defaultProps = {
     logo: true,
     pushmenu: true,
-    styleName: "",
 };
