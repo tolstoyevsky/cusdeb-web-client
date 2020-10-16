a7134fd7be807aa9d15c431cc5c970cb0cfa4939
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-10-16 12:36:08 +0300

    Implement text and count props in Fallback

--- src/common/components/Fallback.jsx
+++ src/common/components/Fallback.jsx
@@ -1,17 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Spinner } from "react-bootstrap";
 
 import Regular from "common/containers/Regular";
 
-const Fallback = () => (
+const Fallback = ({ text, count }) => (
     <Regular>
         <div
-            className="d-flex justify-content-center align-items-center mb-n3"
+            className="d-flex justify-content-center flex-column align-items-center mb-n3"
             style={{ minHeight: "inherit" }}
         >
-            <Spinner animation="border" style={{ width: "4rem", height: "4rem" }} />
+            <Spinner animation="border" style={{ width: "6rem", height: "6rem" }} />
+            {count != null && (
+                <h3 className="position-relative" style={{ marginTop: "-65px" }}>
+                    {count}
+                </h3>
+            )}
+
+            {text && (
+                <h5 className="position-absolute text-center" style={{ marginTop: "200px" }}>
+                    {text}
+                </h5>
+            )}
         </div>
     </Regular>
 );
+
+Fallback.propTypes = {
+    count: PropTypes.number,
+    text: PropTypes.string,
+};
+
+Fallback.defaultProps = {
+    count: null,
+    text: null,
+};
 
 export default Fallback;
