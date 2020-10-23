c81364ae65a802aad863a4f5a9c53626d80bb679
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-10-23 11:56:25 +0300

    Rework Header component using redux store

--- src/common/containers/Header.jsx
+++ src/common/containers/Header.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -9,60 +10,35 @@ import {
     Navbar,
 } from "react-bootstrap";
 
-import * as API from "api/http/users";
 import { signOut } from "utils/users";
 
-export default class Header extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            user: {},
-            userIsAuth: false,
-        };
-    }
-
-    componentDidMount() {
-        API.whoAmI().then((response) => {
-            this.setState(() => ({
-                user: response.data,
-                userIsAuth: response.status === 200,
-            }));
-        });
-    }
-
-    render() {
-        const { children, fluid } = this.props;
-        const { user, userIsAuth } = this.state;
-        return (
-            <Navbar variant="light" className="main-header bg-white" expand="sm">
-                <Container fluid={fluid}>
-                    {children}
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-                            <Nav.Link href="/blog">Blog</Nav.Link>
-                            {userIsAuth ? (
-                                <Dropdown as={Nav.Item} alignRight>
-                                    <Dropdown.Toggle as={Nav.Link}>
-                                        {user.username}
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu className="border-0 shadow">
-                                        <Dropdown.Item href="/settings/profile">Settings</Dropdown.Item>
-                                        <Dropdown.Item onClick={signOut}>Sign out</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            ) : (
-                                <Nav.Link href="/signin">Sign in</Nav.Link>
-                            )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        );
-    }
-}
+const Header = ({ children, fluid, user }) => (
+    <Navbar variant="light" className="main-header bg-white" expand="sm">
+        <Container fluid={fluid}>
+            {children}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                    <Nav.Link href="/blog">Blog</Nav.Link>
+                    {user ? (
+                        <Dropdown as={Nav.Item} alignRight>
+                            <Dropdown.Toggle as={Nav.Link}>
+                                {user.username}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu className="border-0 shadow">
+                                <Dropdown.Item href="/settings/profile">Settings</Dropdown.Item>
+                                <Dropdown.Item onClick={signOut}>Sign out</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    ) : (
+                        <Nav.Link href="/signin">Sign in</Nav.Link>
+                    )}
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+);
 
 Header.propTypes = {
     children: PropTypes.oneOfType([
@@ -73,10 +49,12 @@ Header.propTypes = {
         PropTypes.bool,
         PropTypes.string,
     ]),
+    user: PropTypes.object, // eslint-disable-line react/forbid-prop-types
 };
 
 Header.defaultProps = {
     fluid: null,
+    user: null,
 };
 
 const HeaderLogo = () => (
@@ -101,3 +79,9 @@ const HeaderPushMenu = () => (
 
 Header.Logo = HeaderLogo;
 Header.PushMenu = HeaderPushMenu;
+
+const mapStateToProps = ({ app }) => ({
+    user: app.user,
+});
+
+export default connect(mapStateToProps)(Header);
