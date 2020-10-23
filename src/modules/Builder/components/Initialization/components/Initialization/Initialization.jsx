aac6dd68f14d961c9e7008e6580df644cfd15b28
Denis Gavrilyuk <karpa4o4@gmail.com>
2020-10-23 18:27:01 +0300

    Don't show logs on initialization build stage

--- src/modules/Builder/components/Initialization/components/Initialization/Initialization.jsx
+++ src/modules/Builder/components/Initialization/components/Initialization/Initialization.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import { Card, Spinner } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 import Blackmagic from "api/rpc/blackmagic";
 
 import SelectInitializationParams from "../SelectInitializationParams";
-import { CODE, MSG, BUILD_TYPE_CODES } from "./config";
+import { BUILD_TYPE_CODES } from "./config";
 
 export default class Initialization extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            logs: "",
-
             selectsData: {
                 device: "",
                 os: "",
@@ -44,41 +42,17 @@ export default class Initialization extends Component {
     initializationRPCCallback(event) {
         const { selectsData } = this.state;
         const { builderCallback } = this.props;
-        switch (event) {
-            case CODE.MAINTENANCE_MODE:
-                this.setState(() => ({ logs: MSG.MAINTENANCE_MODE_MSG }));
-                break;
-            case CODE.PREPARE_ENV:
-                this.setState(() => ({ logs: MSG.PREPARE_ENV_MSG }));
-                break;
-            case CODE.MARK_ESSENTIAL_PACKAGES_AS_INSTALLED:
-                this.setState(() => ({ logs: MSG.MARK_ESSENTIAL_PACKAGES_AS_INSTALLED_MSG }));
-                break;
-            case CODE.INSTALL_KEYRING_PACKAGE:
-                this.setState(() => ({ logs: MSG.INSTALL_KEYRING_PACKAGE_MSG }));
-                break;
-            case CODE.UPDATE_INDICES:
-                this.setState(() => ({ logs: MSG.UPDATE_INDICES_MSG }));
-                break;
-            case CODE.UNKNOW_BUILD_TYPE:
-                // Black Magic never returns the error code.
-                break;
-            case CODE.READY:
-                builderCallback();
-                break;
-            case CODE.BUSY:
-                // Black Magic never returns the error code.
-                break;
-            case CODE.LOCKED:
-                // Black Magic never returns the error code.
-                break;
-            default:
-                builderCallback({
-                    state: "initialization",
-                    buildUUID: event,
-                    device: selectsData.device,
-                    os: selectsData.os,
-                });
+
+        // Ready status code
+        if (event === 10) {
+            builderCallback();
+        } else {
+            builderCallback({
+                state: "initialization",
+                buildUUID: event,
+                device: selectsData.device,
+                os: selectsData.os,
+            });
         }
     }
 
@@ -99,18 +73,10 @@ export default class Initialization extends Component {
     }
 
     render() {
-        const { logs } = this.state;
         return (
             <Card>
                 <Card.Body>
-                    {logs ? (
-                        <div className="initialization__logs">
-                            {logs}
-                            <Spinner animation="border" />
-                        </div>
-                    ) : (
-                        <SelectInitializationParams onChange={this.syncSelectsData} />
-                    )}
+                    <SelectInitializationParams onChange={this.syncSelectsData} />
                 </Card.Body>
             </Card>
         );
