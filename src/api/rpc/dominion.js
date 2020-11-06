2a955db02ce0c52e391026bbc63cc64e83d947c3
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-11-06 12:05:50 +0300

    Rename get_rt_build_log dominion rpc to get_build_log

--- src/api/rpc/dominion.js
+++ src/api/rpc/dominion.js
@@ -1,7 +1,7 @@
 import Shirow from "shirow";
 import { dominionUrl } from "../../../config/main"; // TODO: resolve path to config
 
-const GET_RT_BUILD_LOG_RP = "get_rt_build_log";
+const GET_BUILD_LOG_RP = "get_build_log";
 
 export default class Dominion {
     constructor() {
@@ -11,7 +11,7 @@ export default class Dominion {
         }
     }
 
-    async getRtBuildLog(buildUUID, callback) {
-        this.connection.emitForce(GET_RT_BUILD_LOG_RP, buildUUID).then(callback);
+    async getBuildLog(buildUUID, callback) {
+        this.connection.emitForce(GET_BUILD_LOG_RP, buildUUID).then(callback);
     }
 }
