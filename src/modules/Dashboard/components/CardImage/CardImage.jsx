5067b41d14eaa363374e903e801468441ed159fa
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-12-14 16:23:18 +0300

    Implement confirm modal for delete image in Dashboard

--- src/modules/Dashboard/components/CardImage/CardImage.jsx
+++ src/modules/Dashboard/components/CardImage/CardImage.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import { toggleNotesModal, updateModalValue, deleteCurrentImage } from "modules/Dashboard/actions/dashboard";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Modal } from "react-bootstrap";
 
 import {
     faQuestionCircle,
@@ -37,51 +37,73 @@ const CardImage = ({
         Succeeded: faCheck,
     };
 
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+    const handleCloseConfirmModal = () => setShowConfirmModal(false);
+    const handleShowConfirmModal = () => setShowConfirmModal(true);
+
     return (
-        <Card style={{ height: "calc(100% - 1rem)" }}>
-            <Card.Body>
-                <div className="row">
-                    <div className="col-xs-12 col-sm-4 col-md-6">
-                        <h4 className="text-dark">
-                            {`${userImage.distro_name}
-                            for
-                            ${userImage.device_name}
-                            `}
-                        </h4>
-                        <p className="mb-1">{`Build type: ${userImage.flavour_display}`}</p>
-                        <p className="mb-1">
-                            {`Status: ${userImage.status_display}`}
-                            <FontAwesomeIcon icon={statusIcon[userImage.status_display]} className="ml-1" />
-                        </p>
-                        <p className="mb-1">{`Started at: ${startedAtParse(userImage.started_at)}`}</p>
-                        <p className="mb-1">{`Build ID: ${imageId}`}</p>
+        <>
+            <Card style={{ height: "calc(100% - 1rem)" }}>
+                <Card.Body>
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-4 col-md-6">
+                            <h4 className="text-dark">
+                                {`${userImage.distro_name}
+                                for
+                                ${userImage.device_name}
+                                `}
+                            </h4>
+                            <p className="mb-1">{`Build type: ${userImage.flavour_display}`}</p>
+                            <p className="mb-1">
+                                {`Status: ${userImage.status_display}`}
+                                <FontAwesomeIcon icon={statusIcon[userImage.status_display]} className="ml-1" />
+                            </p>
+                            <p className="mb-1">{`Started at: ${startedAtParse(userImage.started_at)}`}</p>
+                            <p className="mb-1">{`Build ID: ${imageId}`}</p>
+                        </div>
+                        {/* <div className="col-xs-12 col-sm-4 col-md-5">
+                            <h4 className="text-dark">Configuraton</h4>
+                            <p>Host name: </p>
+                            <p>Time zone: </p>
+                        </div> */}
                     </div>
-                    {/* <div className="col-xs-12 col-sm-4 col-md-5">
-                        <h4 className="text-dark">Configuraton</h4>
-                        <p>Host name: </p>
-                        <p>Time zone: </p>
-                    </div> */}
-                </div>
-            </Card.Body>
-            <Card.Footer>
-                <Button variant="primary">
-                    <FontAwesomeIcon icon={faDownload} className="mr-1" />
-                    <span className="d-none d-sm-inline">
-                        Download
-                    </span>
-                </Button>
-                <Button variant="primary" className="ml-1" onClick={() => handleOpenModal(imageId, userImage.notes)}>
-                    <FontAwesomeIcon icon={faStickyNote} className="mr-1" />
-                    Add note
-                </Button>
-                <Button variant="primary" className="float-right pl-3" onClick={() => handleDeleteImage(imageId)}>
-                    <FontAwesomeIcon icon={faTrash} className="fa-flip-horizontal mr-1" />
-                    <span className="d-none d-sm-inline">
-                        Delete
-                    </span>
-                </Button>
-            </Card.Footer>
-        </Card>
+                </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary">
+                        <FontAwesomeIcon icon={faDownload} className="mr-1" />
+                        <span className="d-none d-sm-inline">
+                            Download
+                        </span>
+                    </Button>
+                    <Button variant="primary" className="ml-1" onClick={() => handleOpenModal(imageId, userImage.notes)}>
+                        <FontAwesomeIcon icon={faStickyNote} className="mr-1" />
+                        Add note
+                    </Button>
+                    <Button variant="primary" className="float-right pl-3" onClick={handleShowConfirmModal}>
+                        <FontAwesomeIcon icon={faTrash} className="fa-flip-horizontal mr-1" />
+                        <span className="d-none d-sm-inline">
+                            Delete
+                        </span>
+                    </Button>
+                </Card.Footer>
+            </Card>
+
+            <Modal show={showConfirmModal} onHide={handleCloseConfirmModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete image</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to delete this image?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseConfirmModal}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => handleDeleteImage(imageId)}>
+                        Delete image
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 };
 
