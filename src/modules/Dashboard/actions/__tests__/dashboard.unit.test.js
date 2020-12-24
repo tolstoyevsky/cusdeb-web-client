694a71e601669779ad6a0628e025b9f42e1e7930
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-12-24 15:15:10 +0300

    Implement tests for dashboard actions

---
+++ src/modules/Dashboard/actions/__tests__/dashboard.unit.test.js
@@ -0,0 +1,59 @@
+import {
+    deleteImageSucceeded,
+    fetchImagesListSucceeded,
+    hideNotesSucceededMessage,
+    updateNotesSucceeded,
+} from "../dashboard";
+import {
+    DELETE_IMAGE_SUCCEEDED,
+    FETCH_IMAGES_LIST_SUCCEEDED,
+    HIDE_NOTES_SUCCEEDED_MESSAGE,
+    UPDATE_NOTES_SUCCEEDED,
+} from "../../constants/dashboard";
+
+describe("Dashboard actions", () => {
+    it("deleteImageSucceeded", () => {
+        const imageId = "123e4567-e89b-12d3-a456-426614174000";
+        const expectedAction = {
+            type: DELETE_IMAGE_SUCCEEDED,
+            payload: imageId,
+        };
+        expect(deleteImageSucceeded(imageId)).toEqual(expectedAction);
+    });
+
+    it("fetchImagesListSucceeded", () => {
+        const imagesList = [
+            {
+                image_id: "123e4567-e89b-12d3-a456-426614174000",
+                device_name: "devuan-jessie-armhf",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: "2020-12-18T13:18:07.156969Z",
+                status: "Failed",
+                notes: "",
+            },
+        ];
+        const expectedAction = {
+            type: FETCH_IMAGES_LIST_SUCCEEDED,
+            payload: imagesList,
+        };
+        expect(fetchImagesListSucceeded(imagesList)).toEqual(expectedAction);
+    });
+
+    it("hideNotesSucceededMessage", () => {
+        const expectedAction = {
+            type: HIDE_NOTES_SUCCEEDED_MESSAGE,
+        };
+        expect(hideNotesSucceededMessage()).toEqual(expectedAction);
+    });
+
+    it("updateNotesSucceeded", () => {
+        const imageId = "123e4567-e89b-12d3-a456-426614174000";
+        const notes = "Some notes";
+        const expectedAction = {
+            type: UPDATE_NOTES_SUCCEEDED,
+            payload: { imageId, notes },
+        };
+        expect(updateNotesSucceeded({ imageId, notes })).toEqual(expectedAction);
+    });
+});
