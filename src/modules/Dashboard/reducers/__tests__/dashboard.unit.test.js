e308c6bde023b9845d71e6434ad07b1bd3be9ca9
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2020-12-24 15:15:27 +0300

    Implement tests for dashboard reducers

---
+++ src/modules/Dashboard/reducers/__tests__/dashboard.unit.test.js
@@ -0,0 +1,162 @@
+import DashboardReducer from "../dashboard";
+import {
+    deleteImageSucceeded,
+    fetchImagesListSucceeded,
+    hideNotesSucceededMessage,
+    updateNotesSucceeded,
+} from "../../actions/dashboard";
+
+describe("Dashboard reducer", () => {
+    it("Default state", () => {
+        expect(DashboardReducer(undefined, {})).toEqual({
+            images: {},
+            showNotesSucceededMessage: false,
+        });
+    });
+
+    it("DELETE_IMAGE_SUCCEEDED", () => {
+        const deletedImageId = "123e4567-e89b-12d3-a456-426614174000";
+        const images = {
+            "123e4567-e89b-12d3-a456-426614174000": {
+                image_id: "123e4567-e89b-12d3-a456-426614174000",
+                device_name: "devuan-jessie-armhf",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: "2020-12-18T13:18:07.156969Z",
+                status: "Failed",
+                notes: "",
+            },
+            "f99bd97e-ef21-4769-87c6-81a81014c38c": {
+                image_id: "f99bd97e-ef21-4769-87c6-81a81014c38c",
+                device_name: "rpi-zero-w",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: null,
+                status: "Undefined",
+                notes: "",
+            },
+        };
+        const expectedImages = {
+            "f99bd97e-ef21-4769-87c6-81a81014c38c": {
+                image_id: "f99bd97e-ef21-4769-87c6-81a81014c38c",
+                device_name: "rpi-zero-w",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: null,
+                status: "Undefined",
+                notes: "",
+            },
+        };
+        const reducer = DashboardReducer({ images }, deleteImageSucceeded(deletedImageId));
+        expect(reducer).toEqual({ images: expectedImages });
+    });
+
+    it("FETCH_IMAGES_LIST_SUCCEEDED", () => {
+        const imagesList = [
+            {
+                image_id: "123e4567-e89b-12d3-a456-426614174000",
+                device_name: "devuan-jessie-armhf",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: "2020-12-18T13:18:07.156969Z",
+                status: "Failed",
+                notes: "",
+            },
+            {
+                image_id: "f99bd97e-ef21-4769-87c6-81a81014c38c",
+                device_name: "rpi-zero-w",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: null,
+                status: "Undefined",
+                notes: "",
+            },
+        ];
+        const images = {
+            "123e4567-e89b-12d3-a456-426614174000": {
+                image_id: "123e4567-e89b-12d3-a456-426614174000",
+                device_name: "devuan-jessie-armhf",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: "2020-12-18T13:18:07.156969Z",
+                status: "Failed",
+                notes: "",
+            },
+            "f99bd97e-ef21-4769-87c6-81a81014c38c": {
+                image_id: "f99bd97e-ef21-4769-87c6-81a81014c38c",
+                device_name: "rpi-zero-w",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: null,
+                status: "Undefined",
+                notes: "",
+            },
+        };
+        const reducer = DashboardReducer({}, fetchImagesListSucceeded(imagesList));
+        expect(reducer).toEqual({ images });
+    });
+
+    it("HIDE_NOTES_SUCCEEDED_MESSAGE", () => {
+        const reducer = DashboardReducer(
+            { showNotesSucceededMessage: true },
+            hideNotesSucceededMessage(),
+        );
+        expect(reducer).toEqual({ showNotesSucceededMessage: false });
+    });
+
+    it("UPDATE_NOTES_SUCCEEDED", () => {
+        const imageId = "123e4567-e89b-12d3-a456-426614174000";
+        const notes = "Some notes here";
+        const images = {
+            "123e4567-e89b-12d3-a456-426614174000": {
+                image_id: "123e4567-e89b-12d3-a456-426614174000",
+                device_name: "devuan-jessie-armhf",
+                distro_name: "raspbian-buster-armhf",
+                flavour_display: "Classic",
+                started_at: "2020-12-18T13:18:07.156969Z",
+                status_display: "Failed",
+                notes: "",
+            },
+            "f99bd97e-ef21-4769-87c6-81a81014c38c": {
+                image_id: "f99bd97e-ef21-4769-87c6-81a81014c38c",
+                device_name: "rpi-zero-w",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: null,
+                status: "Undefined",
+                notes: "Some notes 2",
+            },
+        };
+        const expectedImages = {
+            "123e4567-e89b-12d3-a456-426614174000": {
+                image_id: "123e4567-e89b-12d3-a456-426614174000",
+                device_name: "devuan-jessie-armhf",
+                distro_name: "raspbian-buster-armhf",
+                flavour_display: "Classic",
+                started_at: "2020-12-18T13:18:07.156969Z",
+                status_display: "Failed",
+                notes,
+            },
+            "f99bd97e-ef21-4769-87c6-81a81014c38c": {
+                image_id: "f99bd97e-ef21-4769-87c6-81a81014c38c",
+                device_name: "rpi-zero-w",
+                distro_name: "raspbian-buster-armhf",
+                flavour: "Classic",
+                started_at: null,
+                status: "Undefined",
+                notes: "Some notes 2",
+            },
+        };
+        const reducer = DashboardReducer(
+            {
+                showNotesSucceededMessage: false,
+                images,
+            },
+            updateNotesSucceeded({ imageId, notes }),
+        );
+        expect(reducer).toEqual({
+            images: expectedImages,
+            showNotesSucceededMessage: true,
+        });
+    });
+});
