8b3bd00c419a3e034d90533a58e3a64df7c8a782
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2021-01-19 16:01:19 +0300

    Implement CusDebLogoCircle

---
+++ src/assets/images/CusDebLogoCircle.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+const CusDebLogoCircle = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={33}
+        height={33}
+        viewBox="0 0 1051.56 1051.56"
+        clipRule="evenodd"
+        fillRule="evenodd"
+        imageRendering="optimizeQuality"
+        shapeRendering="geometricPrecision"
+        textRendering="geometricPrecision"
+        className="brand-image float-none elevation-3 img-circle"
+    >
+        <g transform="translate(-64.27 -65.17)">
+            <circle
+                cx={590.05}
+                cy={590.95}
+                r={525.78}
+                fill="#39404a"
+            />
+            <g>
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M578.13 420.14h-84.31v-84.3h84.31z"
+                />
+                <path
+                    className="prefix__fil2"
+                    fill="#fefefe"
+                    d="M578.09 520.49h-84.3v-84.3h84.3z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M578.07 620.88h-84.31v-84.3h84.31z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M578.07 721.24h-84.3v-84.31h84.3z"
+                />
+                <path
+                    className="prefix__fil2"
+                    fill="#fefefe"
+                    d="M578.07 821.62h-84.3v-84.31h84.3z"
+                />
+                <path
+                    className="prefix__fil2"
+                    fill="#fefefe"
+                    d="M477.8 520.55h-84.31v-84.3h84.31z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M477.77 620.94h-84.3v-84.3h84.3z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M477.78 721.3h-84.3v-84.31h84.3z"
+                />
+                <path
+                    className="prefix__fil2"
+                    fill="#fefefe"
+                    d="M477.78 821.68h-84.3v-84.31h84.3z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M377.48 520.47h-84.31v-84.3h84.31z"
+                />
+                <path
+                    className="prefix__fil2"
+                    fill="#fefefe"
+                    d="M377.45 620.86h-84.3v-84.3h84.3z"
+                />
+                <path
+                    className="prefix__fil2"
+                    fill="#fefefe"
+                    d="M377.46 721.22h-84.31v-84.31h84.31z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M377.46 821.6h-84.31v-84.31h84.31z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M277.15 620.98h-84.31v-84.3h84.31z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M277.15 721.33h-84.3v-84.3h84.3z"
+                />
+                <path
+                    className="prefix__fil1"
+                    fill="#7396c5"
+                    d="M479.52 421.08h-84.3v-84.31h84.3z"
+                />
+                <g>
+                    <path
+                        className="prefix__fil1"
+                        d="M593.9 336.11c97.33-6 264.82 62.85 271.33 220.44 145.04-5.5 187.73 264.43-30.95 264.55l-240.38-.02z"
+                        fill="#7396c5"
+                    />
+                    <path
+                        d="M593.9 437.36c245.57-1.2 255.75 382.04 0 383.73z"
+                        fill="#fefefe"
+                        fillRule="nonzero"
+                    />
+                    <path
+                        d="M593.9 520.88c138.07-.68 143.8 214.81 0 215.75z"
+                        fill="#7396c5"
+                        fillRule="nonzero"
+                    />
+                </g>
+            </g>
+        </g>
+    </svg>
+);
+
+export default CusDebLogoCircle;
