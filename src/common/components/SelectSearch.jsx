b5676288201a6037ff7373054930f1d6c0dd11c9
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2021-02-10 17:37:47 +0300

    Extends SelectSearch from Select instead of Component

--- src/common/components/SelectSearch.jsx
+++ src/common/components/SelectSearch.jsx
@@ -1,9 +1,6 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-
 import Select from "./Select";
 
-export default class SelectSearch extends Component {
+export default class SelectSearch extends Select {
     componentDidMount() {
         const { id, onChange } = this.props;
         $(`#${id}`).select2({
@@ -12,38 +9,4 @@ export default class SelectSearch extends Component {
             onChange(this.value);
         });
     }
-
-    setValue(value) {
-        const { id } = this.props;
-        $(`#${id}`).val(value).trigger("change");
-    }
-
-    render() {
-        const {
-            id, label, options, styleName,
-        } = this.props;
-        return (
-            <Select
-                id={id}
-                label={label}
-                options={options}
-                styleName={styleName}
-            />
-        );
-    }
 }
-
-SelectSearch.propTypes = {
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string,
-    onChange: PropTypes.func,
-    options: PropTypes.arrayOf(PropTypes.object),
-    styleName: PropTypes.string,
-};
-
-SelectSearch.defaultProps = {
-    label: null,
-    onChange: () => { },
-    options: [],
-    styleName: "",
-};
