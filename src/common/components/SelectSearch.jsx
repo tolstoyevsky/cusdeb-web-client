fda3aa887b12c2e4c341e98231636458a3e5e18b
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2021-02-11 16:53:54 +0300

    Add ability to set default value for SelectSearch

--- src/common/components/SelectSearch.jsx
+++ src/common/components/SelectSearch.jsx
@@ -1,12 +1,46 @@
+import PropTypes from "prop-types";
+
 import Select from "./Select";
 
 export default class SelectSearch extends Select {
+    constructor(props) {
+        super(props);
+
+        this.wasSetToDefaultValue = false;
+    }
+
     componentDidMount() {
-        const { id, onChange } = this.props;
+        const { id, onChange, defaultValue } = this.props;
         $(`#${id}`).select2({
             width: "100%",
         }).on("change", function () {
             onChange(this.value);
         });
+
+        if (defaultValue) {
+            this.setDefaultValue();
+        }
+    }
+
+    componentDidUpdate() {
+        const { defaultValue } = this.props;
+
+        if (!this.wasSetToDefaultValue && defaultValue) {
+            this.setDefaultValue();
+        }
+    }
+
+    setDefaultValue() {
+        const { defaultValue, id } = this.props;
+        $(`#${id}`).select2().val(defaultValue).trigger("change");
+        this.wasSetToDefaultValue = true;
     }
 }
+
+SelectSearch.propTypes = {
+    defaultValue: PropTypes.string,
+};
+
+SelectSearch.defaultProps = {
+    defaultValue: null,
+};
