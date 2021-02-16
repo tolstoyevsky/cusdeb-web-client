ed5610546e01071382cc2b203b4ded3565e77b00
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-02-16 18:07:11 +0300

    Rework Builder component

--- src/modules/Builder/components/Builder/components/Builder/Builder.jsx
+++ src/modules/Builder/components/Builder/components/Builder/Builder.jsx
@@ -1,37 +1,56 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
-import { Button, Nav } from "react-bootstrap";
+import * as React from "react";
+import { Col, Nav, Row } from "react-bootstrap";
 import { connect } from "react-redux";
 import {
-    BrowserRouter as Router,
     matchPath,
-    Redirect,
     Route,
+    Router,
     Switch,
+    withRouter,
 } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faMicrochip,
+    faList,
+    faUsers,
+    faCogs,
+    faWrench,
+} from "@fortawesome/free-solid-svg-icons";
 
 import SidebarPage from "common/containers/SidebarPage";
-import { BUILDER_STAGES, BACK_BUTTON_ACTIVE_STAGES, NEXT_BUTTON_INACTIVE_STAGES } from "./config";
+import ChangeStageButton from "../ChangeStageButton/ChangeStageButton";
+import Build from "../../../Build/Build";
+import Configuration from "../../../Configuration/Configuration";
+import Initialization from "../../../Initialization/components/Initialization/Initialization";
+import PackageList from "../../../PackageList/PackageList";
+import Users from "../../../Users/Users";
+import {
+    getStageIndex,
+    stages,
+    toNextStage,
+    toPrevStage,
+} from "../../helpers/stages";
 
-class Builder extends Component {
+class Builder extends React.Component {
+    static isActiveSidebarLink(location) {
+        return matchPath(window.location.pathname, location);
+    }
+
     constructor(props) {
         super(props);
 
-        this.state = {
-            currentStageKey: "initialization",
-            nextButtonIsActive: true,
-            backButtonIsActive: false,
-            buttonState: false,
-        };
+        // The initialization stage should always be the first stage.
+        const { history } = props;
+        if (!matchPath(window.location.pathname, stages.initialization.path)) {
+            history.push(stages.initialization.path);
+        }
 
-        this.currentStageRef = React.createRef();
-        this.waitExecutingStage = false;
+        this.setCurrentStage();
 
-        this.builderCallback = this.builderCallback.bind(this);
-        this.onBack = this.onBack.bind(this);
-        this.onNext = this.onNext.bind(this);
-        this.processStageData = this.processStageData.bind(this);
+        this.onSidebarLinkClick = this.onSidebarLinkClick.bind(this);
+        this.onBackClick = this.onBackClick.bind(this);
+        this.onNextClick = this.onNextClick.bind(this);
     }
 
     componentDidMount() {
@@ -44,191 +63,169 @@ class Builder extends Component {
         });
     }
 
-    onBack() {
-        this.setState((prevState) => {
-            const currentStageIndex = Object.keys(BUILDER_STAGES).findIndex((stageKey) => (
-                stageKey === prevState.currentStageKey
-            ));
-            const backStageKey = Object.keys(BUILDER_STAGES)[currentStageIndex - 1];
-            return {
-                currentStageKey: backStageKey,
-                backButtonIsActive: BACK_BUTTON_ACTIVE_STAGES.includes(backStageKey),
-            };
-        });
-    }
+    onSidebarLinkClick(event) {
+        event.preventDefault();
 
-    onNext() {
-        if (!this.waitExecutingStage) {
-            this.waitExecutingStage = true;
-
-            this.currentStageRef.current.executeState();
-        }
-    }
-
-    builderCallback(stateData) {
-        this.processStageData(stateData);
-    }
-
-    processStageData(data) {
-        switch (data ? data.state : "") {
-            case "buttonState":
-                this.setState(({ buttonState: data.buttonState }));
-                break;
-            default: {
-                // To next state.
-                this.waitExecutingStage = false;
-
-                this.setState((prevState) => {
-                    const currentStageIndex = Object.keys(BUILDER_STAGES).findIndex((stageKey) => (
-                        stageKey === prevState.currentStageKey
-                    ));
-                    const nextStageKey = Object.keys(BUILDER_STAGES)[currentStageIndex + 1];
-                    return {
-                        currentStageKey: nextStageKey,
-                        nextButtonIsActive: !NEXT_BUTTON_INACTIVE_STAGES.includes(nextStageKey),
-                        backButtonIsActive: BACK_BUTTON_ACTIVE_STAGES.includes(nextStageKey),
-                    };
-                });
+        const { dispatch, history } = this.props;
+        const { dataset: { href } } = event.currentTarget;
+        if (!matchPath(window.location.pathname, stages.initialization.path)
+            && !matchPath(window.location.pathname, stages.build.path)) {
+            if (this.currentStage.action) {
+                dispatch(this.currentStage.action(
+                    () => history.push(href),
+                ));
+            } else {
+                history.push(href);
             }
         }
     }
 
+    onBackClick() {
+        const { history } = this.props;
+        toPrevStage(history);
+    }
+
+    onNextClick() {
+        const { dispatch, history } = this.props;
+        if (this.currentStage.action) {
+            dispatch(this.currentStage.action(
+                () => toNextStage(history),
+            ));
+        } else {
+            toNextStage(history);
+        }
+    }
+
+    setCurrentStage() {
+        this.currentStage = Object.values(stages)[getStageIndex()];
+    }
+
     render() {
-        const {
-            currentStageKey,
-            backButtonIsActive,
-            nextButtonIsActive,
-            buttonState,
-        } = this.state;
-        const { buildUUID, deviceShortName, distroShortName } = this.props;
-        const currentStage = BUILDER_STAGES[currentStageKey];
+        this.setCurrentStage();
+
+        const { buildStageAvailable, history } = this.props;
 
         return (
             <div className="builder">
-                <Switch>
-                    <Router>
-                        <Redirect to={currentStage.path} />
-
-                        <SidebarPage collapseSidebar>
-                            <SidebarPage.Sidebar>
-                                {Object.keys(BUILDER_STAGES).map((stageKey) => {
-                                    const stage = BUILDER_STAGES[stageKey];
-                                    return (
-                                        // TODO: rework with using SidebarItem
-                                        <Nav.Item as="li" key={stageKey}>
-                                            <Nav.Link
-                                                active={
-                                                    Boolean(matchPath(currentStage.path, stage))
-                                                }
-                                            >
-                                                <FontAwesomeIcon
-                                                    className={`nav-icon fas ${stage.icon_style}`}
-                                                    icon={stage.icon}
-                                                />
-                                                <p className="sidebar-text collapse show">
-                                                    {stage.title}
-                                                </p>
-                                            </Nav.Link>
-                                        </Nav.Item>
-                                    );
-                                })}
-                            </SidebarPage.Sidebar>
-                            <SidebarPage.Body>
-                                <div className="content-header">
-                                    <div className="container-fluid">
-                                        <Switch>
-                                            {Object.keys(BUILDER_STAGES).map((stageKey) => {
-                                                const stage = BUILDER_STAGES[stageKey];
-                                                return (
-                                                    <Route
-                                                        key={stage.title}
-                                                        path={stage.path}
-                                                        exact
-                                                    >
-                                                        <h3>{stage.title}</h3>
-                                                    </Route>
-                                                );
-                                            })}
-                                        </Switch>
-                                    </div>
-                                </div>
-                                <div className="content">
+                <SidebarPage collapseSidebar>
+                    <SidebarPage.Sidebar>
+                        <Nav.Item>
+                            <Nav.Link
+                                className="nav-link"
+                                active={Builder.isActiveSidebarLink(stages.initialization.path)}
+                            >
+                                <FontAwesomeIcon className="nav-icon fas" icon={faMicrochip} />
+                                <p className="sidebar-text collapse show">Initialization</p>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                data-href={stages.packagelist.path}
+                                className="nav-link"
+                                active={Builder.isActiveSidebarLink(stages.packagelist.path)}
+                                onClick={this.onSidebarLinkClick}
+                            >
+                                <FontAwesomeIcon className="nav-icon fas" icon={faList} />
+                                <p className="sidebar-text collapse show">Package list</p>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                data-href={stages.users.path}
+                                className="nav-link"
+                                active={Builder.isActiveSidebarLink(stages.users.path)}
+                                onClick={this.onSidebarLinkClick}
+                            >
+                                <FontAwesomeIcon className="nav-icon fas" icon={faUsers} />
+                                <p className="sidebar-text collapse show">Users</p>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                data-href={stages.configuration.path}
+                                className="nav-link"
+                                active={Builder.isActiveSidebarLink(stages.configuration.path)}
+                                onClick={this.onSidebarLinkClick}
+                            >
+                                <FontAwesomeIcon className="nav-icon fas" icon={faCogs} />
+                                <p className="sidebar-text collapse show">Configuration</p>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                className="nav-link"
+                                active={Builder.isActiveSidebarLink(stages.build.path)}
+                            >
+                                <FontAwesomeIcon className="nav-icon fas" icon={faWrench} />
+                                <p className="sidebar-text collapse show">Build</p>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </SidebarPage.Sidebar>
+                    <SidebarPage.Body>
+                        <div className="content-header">
+                            <div className="container-fluid">
+                                <h3>
                                     <Switch>
-                                        {Object.keys(BUILDER_STAGES).map((stageKey) => {
-                                            const stage = BUILDER_STAGES[stageKey];
-                                            return (
-                                                <Route
-                                                    key={stage.title}
-                                                    path={stage.path}
-                                                    exact
-                                                >
-                                                    <stage.main
-                                                        ref={
-                                                            matchPath(currentStage.path, stage)
-                                                                ? this.currentStageRef : null
-                                                        }
-                                                        buildUUID={buildUUID}
-                                                        device={deviceShortName}
-                                                        os={distroShortName}
-                                                        builderCallback={this.builderCallback}
-                                                    />
-                                                </Route>
-                                            );
-                                        })}
+                                        <Route path={stages.initialization.path} render={() => "Initialization"} />
+                                        <Route path={stages.packagelist.path} render={() => "Package list"} />
+                                        <Route path={stages.users.path} render={() => "Users"} />
+                                        <Route path={stages.configuration.path} render={() => "Configuration"} />
+                                        <Route path={stages.build.path} render={() => "Build"} />
                                     </Switch>
-                                    <div className="row">
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="content">
+                            <Router history={history}>
+                                <Switch>
+                                    <Route
+                                        path={stages.initialization.path}
+                                        component={Initialization}
+                                    />
+                                    <Route path={stages.packagelist.path} component={PackageList} />
+                                    <Route path={stages.users.path} component={Users} />
+                                    <Route
+                                        path={stages.configuration.path}
+                                        component={Configuration}
+                                    />
+                                    <Route path={stages.build.path} component={Build} />
+                                </Switch>
+                            </Router>
+                            <Row>
+                                <Col xs={6}>
+                                    {this.currentStage.prevStageAvailable && (
                                         <div className="col-6">
-                                            {backButtonIsActive && (
-                                                <div>
-                                                    <Button
-                                                        variant="primary"
-                                                        className="pl-5 pr-5"
-                                                        onClick={this.onBack}
-                                                    >
-                                                        Back
-                                                    </Button>
-                                                </div>
-                                            )}
+                                            <ChangeStageButton onClick={this.onBackClick} text="Back" />
                                         </div>
-                                        <div className="col-6 d-inline-flex justify-content-end">
-                                            {nextButtonIsActive && (
-                                                <Button
-                                                    variant="primary"
-                                                    className="pl-5 pr-5"
-                                                    onClick={this.onNext}
-                                                    disabled={buttonState}
-                                                >
-                                                    Next
-                                                </Button>
-                                            )}
-                                        </div>
+                                    )}
+                                </Col>
+                                {this.currentStage.nextStageAvailable && (
+                                    <div className="col-6 d-inline-flex justify-content-end">
+                                        <ChangeStageButton
+                                            disable={!buildStageAvailable}
+                                            onClick={this.onNextClick}
+                                            text="Next"
+                                        />
                                     </div>
-                                </div>
-                            </SidebarPage.Body>
-                        </SidebarPage>
-                    </Router>
-                </Switch>
+                                )}
+                            </Row>
+                        </div>
+                    </SidebarPage.Body>
+                </SidebarPage>
             </div>
         );
     }
 }
 
 Builder.propTypes = {
-    buildUUID: PropTypes.string,
-    deviceShortName: PropTypes.string,
-    distroShortName: PropTypes.string,
+    buildStageAvailable: PropTypes.bool.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
+    history: PropTypes.object.isRequired,
 };
 
-Builder.defaultProps = {
-    buildUUID: null,
-    deviceShortName: null,
-    distroShortName: null,
-};
-
-const mapStateToProps = ({ initialization }) => ({
-    buildUUID: initialization.buildUUID,
-    deviceShortName: initialization.deviceShortName,
-    distroShortName: initialization.distroShortName,
+const mapStateToProps = ({ builder }) => ({
+    buildStageAvailable: builder.buildStageAvailable,
 });
 
-export default connect(mapStateToProps)(Builder);
+export default withRouter(connect(mapStateToProps)(Builder));
