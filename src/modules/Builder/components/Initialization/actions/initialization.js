14dbadcc96d56ae5ba230a4a3a2b60fed9683817
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-02-16 17:32:01 +0300

    Implement saga handlers for initializing new and exising image

--- src/modules/Builder/components/Initialization/actions/initialization.js
+++ src/modules/Builder/components/Initialization/actions/initialization.js
@@ -1,7 +1,9 @@
 import { createAction } from "redux-actions";
 
 import {
+    EXECUTE_STAGE,
     FETCH_DEVICE_LIST,
+    INIT_EXISTING_IMAGE,
     SET_BUILD_TYPE,
     SET_BUILD_TYPE_LIST,
     SET_BUILD_UUID,
@@ -12,7 +14,11 @@ import {
     TOGGLE_CONTINUE_BUILD_MODAL,
 } from "../constants/initialization";
 
+export const executeStage = createAction(EXECUTE_STAGE);
+
 export const fetchDeviceList = createAction(FETCH_DEVICE_LIST);
+
+export const initExistingImage = createAction(INIT_EXISTING_IMAGE);
 
 export const setBuildType = createAction(SET_BUILD_TYPE);
 
--- src/modules/Builder/components/Initialization/sagas/initialization.js
+++ src/modules/Builder/components/Initialization/sagas/initialization.js
@@ -3,24 +3,71 @@ import {
     call,
     fork,
     put,
+    select,
     takeEvery,
 } from "redux-saga/effects";
 
 import { listDevice } from "api/http/images";
-import { setDeviceList } from "../actions/initialization";
-import { FETCH_DEVICE_LIST } from "../constants/initialization";
+import Blackmagic from "api/rpc/blackmagic";
+import { setBuildUUID, setDeviceList } from "../actions/initialization";
+import { lastBuildUUDKey } from "../components/Initialization/Initialization";
+import { EXECUTE_STAGE, FETCH_DEVICE_LIST, INIT_EXISTING_IMAGE } from "../constants/initialization";
+
+const getInitialization = (state) => state.initialization;
+
+const BUILD_TYPE_CODES = {
+    "Classic image": 1,
+    "Mender-compatible image": 2,
+    "Mender artifact": 3,
+};
+
+function* executeStage({ payload: callback }) {
+    const { deviceShortName, distroShortName, buildType } = yield select(getInitialization);
+
+    const blackmagic = new Blackmagic();
+    const buildUUID = yield blackmagic.initNewImage(
+        "My image",
+        deviceShortName,
+        distroShortName,
+        BUILD_TYPE_CODES[buildType],
+    );
+
+    window.localStorage.setItem(lastBuildUUDKey, buildUUID);
+    yield put(setBuildUUID(buildUUID));
+
+    return callback();
+}
 
 function* fetchListDevice() {
     const { data: deviceList } = yield call(listDevice);
     yield put(setDeviceList(deviceList));
 }
 
+function* initExistingImage({ payload: callback }) {
+    const blackmagic = new Blackmagic();
+    const lastBuildUUID = window.localStorage.getItem(lastBuildUUDKey);
+
+    yield blackmagic.initExistingImage(lastBuildUUID);
+
+    return callback();
+}
+
+function* watchExecuteStage() {
+    yield takeEvery(EXECUTE_STAGE, executeStage);
+}
+
 function* watchFetchListDevice() {
     yield takeEvery(FETCH_DEVICE_LIST, fetchListDevice);
 }
 
+function* watchInitExistingImage() {
+    yield takeEvery(INIT_EXISTING_IMAGE, initExistingImage);
+}
+
 export default function* builderSaga() {
     yield all([
+        fork(watchExecuteStage),
         fork(watchFetchListDevice),
+        fork(watchInitExistingImage),
     ]);
 }
