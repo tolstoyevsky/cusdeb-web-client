d10ac991c69a1233d1d39a92009418d89568fe22
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2021-02-18 16:01:14 +0300

    Implement updateBuildStageAvailable action in Builder

--- src/modules/Builder/components/Builder/reducers/builder.js
+++ src/modules/Builder/components/Builder/reducers/builder.js
@@ -1,7 +1,17 @@
 import { handleActions } from "redux-actions";
 
+import { UPDATE_BUILD_STAGE_AVAILABLE } from "../constants/builder";
+
 const defaultState = {
     buildStageAvailable: true,
 };
 
-export default handleActions({}, defaultState);
+export default handleActions(
+    {
+        [UPDATE_BUILD_STAGE_AVAILABLE]: (state, { payload: buildStageAvailable }) => ({
+            ...state,
+            buildStageAvailable,
+        }),
+    },
+    defaultState,
+);
