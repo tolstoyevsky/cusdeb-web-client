1ae814105d373673c41fe085c0665e78546a46ee
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-03-23 13:34:27 +0300

    Add tests for CardImage and NotesModal components

---
+++ src/modules/Dashboard/components/CardImage/__tests__/CardImage.test.jsx
@@ -0,0 +1,131 @@
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import * as React from "react";
+import { Button, Modal } from "react-bootstrap";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { cleanup } from "@testing-library/react";
+
+import CardImage from "../CardImage";
+import ConfirmDeleteModal from "../../ConfirmDeleteModal/ConfirmDeleteModal";
+import DownloadInfoModal from "../../DownloadInfoModal/DownloadInfoModal";
+import NotesModal from "../../NotesModal/NotesModal";
+
+configure({ adapter: new Adapter() });
+
+afterEach(cleanup);
+
+describe("CardImage", () => {
+    const mockStore = configureStore([]);
+    const deviceList = {
+        "rpi-4-b": {
+            name: "Raspberry Pi",
+            generation: 4,
+            model: "Model B",
+            distros: {
+                "raspbian-buster-armhf": {
+                    name: "Raspbian",
+                    codename: "Buster",
+                    version: 10,
+                    port: "armhf",
+                    packages_url: "https://packages.debian.org/buster/",
+                    build_types: ["classic"],
+                },
+            },
+        },
+    };
+    const image = (status = "Undefined") => ({
+        image_id: "xxxxxxxx-xxxx-4xxx-xxxx-xxxxxxxxxxxx",
+        device_name: "rpi-4-b",
+        distro_name: "raspbian-buster-armhf",
+        flavour: "Classic",
+        started_at: null,
+        status,
+        notes: "",
+    });
+
+    const render = ({ imageStatus }) => {
+        const initialState = {
+            dashboard: {
+                showNotesSucceededMessage: false,
+            },
+            initialization: {
+                deviceList,
+            },
+        };
+        const store = mockStore(initialState);
+
+        return mount(
+            <Provider store={store}>
+                <CardImage
+                    image={image(imageStatus)}
+                />
+            </Provider>,
+        );
+    };
+
+    /**
+     * Checks the 'href' and 'download' attributes of the download button.
+     */
+    it("Download Button attributes is valid", () => {
+        const component = render({ imageStatus: "Succeeded" });
+        const downloadButton = component.find(Button).at(0);
+        expect(downloadButton.prop("href")).toBe(`/download-image/${image().image_id}.img.gz`);
+        expect(downloadButton.prop("download")).toBe(true);
+    });
+
+    /**
+     * Checks if the modal window for downloading an image is shown when the status of the image
+     * is Undefined.
+     */
+    it("Download modal is shown when image status is Undefined", () => {
+        const component = render({});
+
+        const hiddenDownloadImageModal = component.find(DownloadInfoModal).find(Modal);
+        const nestedHiddenDownloadImageModal = hiddenDownloadImageModal.children();
+        expect(nestedHiddenDownloadImageModal.children().length).toEqual(0);
+
+        const addNotesButton = component.find(Button).at(0);
+        addNotesButton.simulate("click");
+
+        const showedDownloadImageModal = component.find(DownloadInfoModal).find(Modal);
+        const nestedShowedDownloadImageModal = showedDownloadImageModal.children();
+        expect(nestedShowedDownloadImageModal.children().length).toBeGreaterThan(0);
+    });
+
+    /**
+     * Checks if the modal window for adding notes is shown.
+     */
+    it("Add notes modal is shown", () => {
+        const component = render({});
+
+        const hiddenNotesModal = component.find(NotesModal).find(Modal);
+        const nestedHiddenNotesModal = hiddenNotesModal.children();
+        expect(nestedHiddenNotesModal.children().length).toEqual(0);
+
+        const addNotesButton = component.find(Button).at(1);
+        addNotesButton.simulate("click");
+
+        const showedNotesModal = component.find(NotesModal).find(Modal);
+        const nestedShowedHiddenNotesModal = showedNotesModal.children();
+        expect(nestedShowedHiddenNotesModal.children().length).toBeGreaterThan(0);
+    });
+
+    /**
+     * Checks if the modal window for deleting image is shown.
+     */
+    it("Delete image modal is shown", () => {
+        const component = render({});
+
+        const hiddenDeleteImageModal = component.find(ConfirmDeleteModal).find(Modal);
+        const nestedHiddenDeleteImageModal = hiddenDeleteImageModal.children();
+        expect(nestedHiddenDeleteImageModal.children().length).toEqual(0);
+
+        const addNotesButton = component.find(Button).at(2);
+        addNotesButton.simulate("click");
+
+        const showedDeleteImageModal = component.find(ConfirmDeleteModal).find(Modal);
+        const nestedShowedDeleteImageModal = showedDeleteImageModal.children();
+        expect(nestedShowedDeleteImageModal.children().length).toBeGreaterThan(0);
+    });
+});
---
+++ src/modules/Dashboard/components/NotesModal/__tests__/NotesModal.test.jsx
@@ -0,0 +1,92 @@
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import * as React from "react";
+import { Alert, Modal } from "react-bootstrap";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { cleanup } from "@testing-library/react";
+
+import NotesModal from "../NotesModal";
+
+configure({ adapter: new Adapter() });
+
+afterEach(cleanup);
+
+describe("NotesModal", () => {
+    const imageId = "xxxxxxxx-xxxx-4xxx-xxxx-xxxxxxxxxxxx";
+    const mockStore = configureStore([]);
+
+    const render = ({ initialValue, show = true, showNotesSucceededMessage = false }) => {
+        const initialState = {
+            dashboard: {
+                showNotesSucceededMessage,
+            },
+        };
+        const store = mockStore(initialState);
+
+        return mount(
+            <Provider store={store}>
+                <NotesModal
+                    handleClose={jest.fn()}
+                    show={show}
+                    imageId={imageId}
+                    initialValue={initialValue}
+                />
+            </Provider>,
+        );
+    };
+
+    /**
+     * Checks if Modal is rendered when the show prop is true.
+     */
+    it("Modal rendered", () => {
+        const component = render({});
+        expect(component.find(Modal).length).toEqual(1);
+    });
+
+    /**
+     * Checks if Modal isn't rendered when the show prop is false.
+     */
+    it("Modal isn't rendered", () => {
+        const component = render({ show: false });
+        const modal = component.find(Modal);
+        const nestedModal = modal.children();
+        expect(nestedModal.children().length).toEqual(0);
+    });
+
+    /**
+     * Checks if the succeeded alert is shown when the showNotesSucceededMessage prop is true.
+     */
+    it("Succeeded alert is shown", () => {
+        const component = render({ showNotesSucceededMessage: true });
+        expect(component.find(Alert).prop("show")).toEqual(true);
+    });
+
+    /**
+     * Checks if the succeeded alert isn't shown when the showNotesSucceededMessage prop is false.
+     */
+    it("Succeeded alert isn't shown", () => {
+        const component = render({});
+        expect(component.find(Alert).prop("show")).toEqual(false);
+    });
+
+    /**
+     * Checks if the specified initial value is used in the textarea field by default.
+     */
+    it("Specify initial value for textarea", () => {
+        const initialValue = "Test value";
+        const component = render({ initialValue });
+        expect(component.find("textarea").prop("value")).toEqual(initialValue);
+    });
+
+    /**
+     * Checks entering a value into the textarea field.
+     */
+    it("Enter value into textarea", () => {
+        const value = "Test value";
+        const component = render({});
+        const textarea = component.find("textarea");
+        textarea.simulate("change", { target: { value } });
+        expect(component.find("textarea").prop("value")).toEqual(value);
+    });
+});
