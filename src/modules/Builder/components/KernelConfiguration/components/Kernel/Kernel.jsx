308bd637c998fc5bd09f3c27bc81342de6c05655
Themanwhosmellslikesugar <tmwsls12@gmail.com>
2021-06-09 23:27:27 +0300

    Show warning if distro is not supported kernel configuration

--- src/modules/Builder/components/KernelConfiguration/components/Kernel/Kernel.jsx
+++ src/modules/Builder/components/KernelConfiguration/components/Kernel/Kernel.jsx
@@ -7,48 +7,71 @@ import { withRouter } from "react-router-dom";
 import KernelConfiguration from "../KernelConfiguration/KernelConfiguration";
 import { fetchKernelConfiguratorThemes, initNewKernelConfiguration } from "../../actions/kernelConfiguration";
 
-const Kernel = ({ fetchKernelConfiguratorThemesAction, initNewKernelConfigurationAction }) => {
+const supportedDistros = ["ubuntu-focal-armhf"];
+
+const Kernel = ({
+    distroShortName,
+    fetchKernelConfiguratorThemesAction,
+    initNewKernelConfigurationAction,
+}) => {
     const [isConfigure, setIsConfigure] = useState(false);
 
     useEffect(() => {
-        fetchKernelConfiguratorThemesAction();
-        initNewKernelConfigurationAction();
-    }, []);
+        if (supportedDistros.includes(distroShortName)) {
+            fetchKernelConfiguratorThemesAction();
+            initNewKernelConfigurationAction();
+        }
+    }, [distroShortName]);
+
+    if (supportedDistros.includes(distroShortName)) {
+        return (
+            <Card>
+                <Card.Header>
+                    <Form>
+                        <Form.Check
+                            id="default"
+                            name="default"
+                            type="radio"
+                            label="Default distro kernel configuration"
+                            onChange={() => setIsConfigure(false)}
+                            defaultChecked
+                        />
+                        <Form.Check
+                            id="configure"
+                            name="default"
+                            type="radio"
+                            label="Configure kernel"
+                            onChange={() => setIsConfigure(true)}
+                        />
+                    </Form>
+                </Card.Header>
+                <Collapse in={isConfigure} timeout={500}>
+                    <Card.Body>
+                        <KernelConfiguration />
+                    </Card.Body>
+                </Collapse>
+            </Card>
+        );
+    }
 
     return (
-        <Card>
-            <Card.Header>
-                <Form>
-                    <Form.Check
-                        id="default"
-                        name="default"
-                        type="radio"
-                        label="Default distro kernel configuration"
-                        onChange={() => setIsConfigure(false)}
-                        defaultChecked
-                    />
-                    <Form.Check
-                        id="configure"
-                        name="default"
-                        type="radio"
-                        label="Configure kernel"
-                        onChange={() => setIsConfigure(true)}
-                    />
-                </Form>
-            </Card.Header>
-            <Collapse in={isConfigure} timeout={500}>
-                <Card.Body>
-                    <KernelConfiguration />
-                </Card.Body>
-            </Collapse>
-        </Card>
+        <div className="alert alert-info">
+            This distro is not yet supported, if you want to be able to configure the
+            Linux kernel for this distribution, write to&nbsp;
+            <a href="mailto:info@cusdeb.com">info@cusdeb.com</a>
+        </div>
     );
 };
 
 Kernel.propTypes = {
+    distroShortName: PropTypes.string.isRequired,
     fetchKernelConfiguratorThemesAction: PropTypes.func.isRequired,
     initNewKernelConfigurationAction: PropTypes.func.isRequired,
 };
+
+const mapStateToProps = ({ initialization }) => ({
+    distroShortName: initialization.distroShortName,
+});
 
 const mapDispatchToProps = (dispatch) => ({
     fetchKernelConfiguratorThemesAction: () => dispatch(fetchKernelConfiguratorThemes()),
@@ -56,5 +79,5 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default withRouter(
-    connect(null, mapDispatchToProps)(Kernel),
+    connect(mapStateToProps, mapDispatchToProps)(Kernel),
 );
